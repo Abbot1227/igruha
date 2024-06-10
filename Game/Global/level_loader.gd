9f9aabf8198c2be479bcfeeef47016bafaff014d
unknown <tengri123ax@gmail.com>
2024-06-10 21:03:13 +0900

    dynamic level loading

---
+++ Game/Global/level_loader.gd
@@ -0,0 +1,14 @@
+class_name LevelLoader extends Node
+
+var active_levels = {}
+var current_level
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
